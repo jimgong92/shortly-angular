d113c7122cfef21cd9cf51d94b0916ca9df26172
jimgong92 <gong.jim@gmail.com>
2014-11-24 17:23:00 -0800

    Passes tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,18 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  // whaddafu?
+  $scope.data = {};
+  $scope.getLinks = function(){
+    // for each link in links make property $scope.data.link = link;
+    Links.get()
+    .then(function(links){
+      $scope.data.links = links;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  };
+  $scope.getLinks();
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,14 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+  $scope.addLink = function(link) {
+    Links.add(link)
+    .then(function(data){
+      console.log(data);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  };
 });
